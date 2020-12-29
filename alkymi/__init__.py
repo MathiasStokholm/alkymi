3701ef18cb48d418ebd8ea49653755bec6c9e653
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2020-12-29 13:38:36 +0100

    Added version

--- alkymi/__init__.py
+++ alkymi/__init__.py
@@ -4,3 +4,7 @@ from .lab import Lab
 from .logging import log
 from .decorators import recipe, foreach
 from .recipes import *
+
+# Define version
+from .version import VERSION
+__version__ = VERSION
