973e00b527f76f2164fdae9652fa171a97fa2197
Mathias Bøgh Stokholm <mathias.stokholm@gmail.com>
2024-03-28 20:04:18 +0100

    Introduced live streaming of stderr to alk.call()

--- alkymi/utils.py
+++ alkymi/utils.py
@@ -1,7 +1,10 @@
 import asyncio
+import fcntl
+import os
 import subprocess
 import sys
 import threading
+import time
 from typing import List, TextIO, Optional, TypeVar, Callable
 
 from .logging import log
@@ -14,7 +17,7 @@ def call(args: List[str], echo_error_to_stream: Optional[TextIO] = sys.stderr,
     log)
 
     :param args: The arguments representing the command to run, e.g. ["echo", "test"]
-    :param echo_error_to_stream: A stream to which to echo the call's stderr on a non-zero exit code
+    :param echo_error_to_stream: A stream to which to echo the call's stderr while the command is executing
     :param echo_output_to_stream: A stream to which to echo the call's stdout while the command is executing
     :return: The result of the execution as a subprocess.CompletedProcess instance
     """
@@ -22,42 +25,72 @@ def call(args: List[str], echo_error_to_stream: Optional[TextIO] = sys.stderr,
     if echo_output_to_stream is not None and getattr(echo_output_to_stream, "name", None) == sys.stdout.name:
         echo_output_to_stream = sys.stdout
 
+    # If running through a Lab CLI, sys.stderr may have been redirected
+    if echo_error_to_stream is not None and getattr(echo_error_to_stream, "name", None) == sys.stderr.name:
+        echo_error_to_stream = sys.stderr
+
+    live_stdout = echo_output_to_stream is not None
+    live_stderr = echo_error_to_stream is not None
+
     # Buffer one line at a time if echoing live, otherwise just use the default
-    buffer_size = 1 if echo_output_to_stream is not None else -1
+    buffer_size = 1 if (live_stdout or live_stderr) else -1
     proc = subprocess.Popen(args, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                             bufsize=buffer_size)
+
+    # If running either stdout or stderr live, set the streams to non-blocking mode to ensure that we don't block
+    # when trying to read from either of them
+    if live_stdout:
+        fl = fcntl.fcntl(proc.stdout, fcntl.F_GETFL)
+        fcntl.fcntl(proc.stdout, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+    if live_stderr:
+        fl = fcntl.fcntl(proc.stderr, fcntl.F_GETFL)
+        fcntl.fcntl(proc.stderr, fcntl.F_SETFL, fl | os.O_NONBLOCK)
 
     # Since we set these to PIPE, these should never be None
     assert proc.stdout is not None
     assert proc.stderr is not None
 
     stdout: str = ""
-    if echo_output_to_stream is not None:
+    stderr: str = ""
+    if live_stdout or live_stderr:
         # Print each line to the stream as it arrives
         while proc.poll() is None:
-            line = proc.stdout.readline()
-            echo_output_to_stream.write(line)
-            stdout += line
+            if live_stdout:
+                line = proc.stdout.readline()
+                echo_output_to_stream.write(line)
+                stdout += line
+            if live_stderr:
+                line = proc.stderr.readline()
+                echo_error_to_stream.write(line)
+                stderr += line
 
-        # Program has finished executing, check if any part of stdout still needs to be piped
-        line = proc.stdout.readline()
-        if line:
-            echo_output_to_stream.write(line)
-            stdout += line
+            # Sleep for a tiny bit to ensure that the non-blocking calls don't eat a ton of CPU
+            time.sleep(0.001)
+
+        # Program has finished executing, check if any part of stdout or stderr still needs to be piped
+        if live_stdout:
+            line = proc.stdout.readline()
+            if line:
+                echo_output_to_stream.write(line)
+                stdout += line
+        # if live_stderr:
+        #     line = proc.stderr.readline()
+        #     if line:
+        #         echo_error_to_stream.write(line)
+        #         stderr += line
     else:
         # Otherwise, simply wait for the command to finish and then grab stdout
         proc.wait()
-        stdout = proc.stdout.read()
 
-    stderr = proc.stderr.read()
+    # If not running live, read everything in one go
+    if not live_stdout:
+        stdout = proc.stdout.read()
+    if not live_stderr:
+        stderr = proc.stderr.read()
 
     # Send to alkymi log
     log.debug(stdout)
     log.debug(stderr)
-
-    # Always forward error messages to stderr if needed
-    if echo_error_to_stream is not None and proc.returncode != 0:
-        echo_error_to_stream.write(stderr)
 
     # Raise an error on failure
     if proc.returncode != 0:
